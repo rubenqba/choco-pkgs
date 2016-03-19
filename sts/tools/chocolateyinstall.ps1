ef3e01789121ecc67b82c29bb57329be1a274f2e
Rubén Bresler Camps <ruben1981@gmail.com>
2016-03-18 21:47:45 -0400

    subiendo versión de STS

--- sts/tools/chocolateyinstall.ps1
+++ sts/tools/chocolateyinstall.ps1
@@ -2,11 +2,11 @@
 
 
 $packageName= 'STS' # arbitrary name for the package, used in messages
-$packageVersion = '3.7.2'
+$packageVersion = '3.7.3'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'http://dist.springsource.com/release/STS/3.7.2.RELEASE/dist/e4.5/spring-tool-suite-3.7.2.RELEASE-e4.5.1-win32.zip'
-$url64      = 'http://dist.springsource.com/release/STS/3.7.2.RELEASE/dist/e4.5/spring-tool-suite-3.7.2.RELEASE-e4.5.1-win32-x86_64.zip'
-#$url64      = 'http://172.24.9.167/resources/sts/v3.7.2/spring-tool-suite-3.7.2.RELEASE-e4.5.1-win32-x86_64.zip'
+$url        = 'http://dist.springsource.com/release/STS/$packageVersion.RELEASE/dist/e4.5/spring-tool-suite-$packageVersion.RELEASE-e4.5.1-win32.zip'
+$url64      = 'http://dist.springsource.com/release/STS/$packageVersion.RELEASE/dist/e4.5/spring-tool-suite-$packageVersion.RELEASE-e4.5.1-win32-x86_64.zip'
+#$url64      = 'http://172.24.9.167/resources/sts/v$packageVersion/spring-tool-suite-$packageVersion.RELEASE-e4.5.1-win32-x86_64.zip'
 $global:installLocation = "C:\tools\SpringToolSuite\v$packageVersion"
 
 $checksum64     = "27e597792ff04392d2f255814716e8a7d4150b26"
@@ -18,7 +18,7 @@ if(!$PSScriptRoot){ $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Par
 . "$PSScriptRoot\UtilFunctions.ps1"
 
 #Parse-Parameters
-Install-ZipPackage $packageName $url $global:installLocation $url64 -specificFolder "sts-bundle\sts-3.7.2.RELEASE\*" -checksum64 "$checksum64" -checksumType64 "sha1"
+Install-ZipPackage $packageName $url $global:installLocation $url64 -specificFolder "sts-bundle\sts-$packageVersion.RELEASE\*" -checksum64 "$checksum64" -checksumType64 "sha1"
 $finalLocation = $(Join-Path $global:installLocation "STS")
 Move-Item $(Join-Path $global:installLocation "sts-bundle\sts-$packageVersion.RELEASE") $finalLocation -Force
 Remove-Item $(Join-Path $global:installLocation "sts-bundle") -Force -Recurse
